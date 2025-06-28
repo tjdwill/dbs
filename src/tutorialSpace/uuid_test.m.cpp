95eb1c6e46aba917e704408aa0078b7988845b67
Terrance Williams <tjdwill.gh@gmail.com>
2025-06-28 10:10:29 -0600

    add uuid comparison test

--- src/tutorialSpace/uuid_test.m.cpp
+++ src/tutorialSpace/uuid_test.m.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <functional>
 #include <iostream>
 #include <random>
@@ -13,10 +14,15 @@ int main()
   auto seedData = std::array<int, std::mt19937::state_size> {};
   std::generate(std::begin(seedData), std::end(seedData), std::ref(rd));
   std::seed_seq seq(std::begin(seedData), std::end(seedData));
-  std::mt19937 generator(seq);
+  std::mt19937 generator { seq };
   uuids::uuid_random_generator uuidGenerator { generator };
 
+  using namespace uuids;
   for (auto _ : std::views::iota(0, 10)) {
-    std::cout << uuidGenerator() << "\n";
+    uuid currentUuid = uuidGenerator();
+    std::cout << currentUuid << "\n";
+    uuid const testUuid =
+      uuid::from_string(uuids::to_string(currentUuid)).value();
+    assert(currentUuid == testUuid);
   }
 }
