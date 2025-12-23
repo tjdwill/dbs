7c8f7820f8fde9f1ca2bba31b4f382633865ee6c
Terrance Williams <tjdwill.gh@gmail.com>
2025-12-22 22:31:28 -0700

    dbsutl shared library symbol visibility

--- src/dbsutl/dbsutl_helpers.h
+++ src/dbsutl/dbsutl_helpers.h
@@ -1,19 +1,27 @@
 // dbsutl_helpers.h
+
+#ifndef INCLUDED_DBSUTL_HELPERS
+#define INCLUDED_DBSUTL_HELPERS
+
 //@PURPOSE: Provide helper functions that facilitate code implementation.
 //
 //@DESCRIPTION: This component serves as a housing for useful functions.
 //  At the time of writing, there is no defined structure due to a lack of quantity,
 //  but note that functions may be to be moved around over time.
+#include <dbsutl_sharedapi.h>
+
 #include <concepts>
 
 namespace dbsutl {
 
 template< typename EnumType, std::integral ReturnType = int >
-constexpr auto enumAsIntegral( EnumType type ) -> ReturnType
+DBSUTL_API constexpr auto enumAsIntegral( EnumType type ) -> ReturnType
 {
   return static_cast< ReturnType >( type );
 }
 } // namespace dbsutl
+
+#endif // include guard
 
 // -----------------------------------------------------------------------------
 // Copyright (C) 2025 Terrance Williams
