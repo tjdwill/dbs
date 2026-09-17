087f34f432d017c5e7ad53b532309179ed13c697
Terrance Williams <tjdwill.gh@gmail.com>
2026-09-17 10:24:56 -0600

    Automate installation of header-only libraries

--- utilities/install3rdparty.sh
+++ utilities/install3rdparty.sh
@@ -4,13 +4,26 @@
 git submodule update --init
 
 # stduuid, tomlplusplus
-cd 3rdparty
-for repo in stduuid tomlplusplus
-do 
-    buildDir="_lib/_temp$repo"
-    mkdir -p "$buildDir"
-    cmake -S "$repo" -B "$buildDir"
-    cmake --install "$buildDir" --prefix=$(realpath _lib)
-    rm -rf "$buildDir"
-done
-unset buildDir
+installHeaderOnlyLibraries()
+{
+    cd 3rdparty
+    for repo in stduuid tomlplusplus
+    do 
+        echo "Installing ${repo}"
+        local buildDir="_lib/_temp$repo"
+        mkdir -p "$buildDir"
+        cmake -S "$repo" -B "$buildDir"
+        cmake --install "$buildDir" --prefix=$(realpath _lib)/"$repo"
+        rm -rf "$buildDir"
+    done
+}
+
+buildAndInstallBde()
+{
+    echo -e "\nThe BDE libraries must currently be built separately due to different levels of configuration. See BUILD.md"
+}
+
+# ---
+
+installHeaderOnlyLibraries
+buildAndInstallBde
