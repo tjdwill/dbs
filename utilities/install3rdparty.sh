d2d352fd0b05be5f23125c2715731f5940253de5
Terrance Williams <tjdwill.gh@gmail.com>
2026-09-17 10:08:29 -0600

    Begin creating utility script for easy 3rdparty installation

---
+++ utilities/install3rdparty.sh
@@ -0,0 +1,16 @@
+# Must run from top-level project directory
+
+# Ensure submodules are initialized
+git submodule update --init
+
+# stduuid, tomlplusplus
+cd 3rdparty
+for repo in stduuid tomlplusplus
+do 
+    buildDir="_lib/_temp$repo"
+    mkdir "$buildDir"
+    cmake -S "$repo" -B "$buildDir"
+    cmake --install "$buildDir" --prefix=$(realpath _lib)
+    rm -rf "$buildDir"
+done
+unset buildDir
