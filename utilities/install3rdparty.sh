c3b41735109e30e960bb4d72d930a24f3f6103d9
Terrance Williams <tjdwill.gh@gmail.com>
2026-09-17 10:56:01 -0600

    Add automated BDE build using default profile

--- utilities/install3rdparty.sh
+++ utilities/install3rdparty.sh
@@ -3,14 +3,16 @@
 # Ensure submodules are initialized
 git submodule update --init
 
+topLevelProject=$(pwd)
+
 # stduuid, tomlplusplus
 installHeaderOnlyLibraries()
 {
-    cd 3rdparty
+    cd "$topLevelProject/3rdparty"
     for repo in stduuid tomlplusplus
     do 
         echo "Installing ${repo}"
-        local buildDir="_lib/_temp$repo"
+        local buildDir="_lib/_tmp$repo"
         mkdir -p "$buildDir"
         cmake -S "$repo" -B "$buildDir"
         cmake --install "$buildDir" --prefix=$(realpath _lib)/"$repo"
@@ -18,9 +20,27 @@ installHeaderOnlyLibraries()
     done
 }
 
+# Build and Install BDE for all three configurations using the default profile
 buildAndInstallBde()
 {
-    echo -e "\nThe BDE libraries must currently be built separately due to different levels of configuration. See BUILD.md"
+    cd "$topLevelProject/3rdparty"
+    local oldPATH="$PATH"
+    export PATH=$(realpath bde-tools/bin):"$PATH"
+    
+    local configurationsMap=([Debug]=dbg [Release]=opt [RelWithDebInfo]=opt_dbg)
+    local buildDirPrefix="$topLevelProject/3rdparty/_lib/_tmpBde"
+    local installDirPrefix="$topLevelProject/3rdparty/_lib/bde"
+    cd bde
+    for config in ${!configurationsMap[@]}
+    do
+        echo "Building and installing BDE ${config} configuration."
+        local ufidString="${configurationsMap[$config]}_64_cpp23_pic"
+        eval `bbs_build_env --install-dir "$installDirPrefix/$config" --build-dir "$buildDirPrefix/$config" --ufid "${ufidString}"`
+        bbs_build configure --clean
+        bbs_build build
+        bbs_build install
+    done
+    rm -rf "$buildDirPrefix"
 }
 
 # ---
